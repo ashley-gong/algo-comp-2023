3221c8ee6f106092335121ba281c947ebd4c3643
Ashley Gong <ashleygong@college.harvard.edu>
2022-11-07 00:22:41 -0500

    whattttt

--- assignment2/match.py
+++ assignment2/match.py
@@ -1,6 +1,18 @@
 import numpy as np
 from typing import List, Tuple
 
+
+def pref_list(gender_preference):
+    if gender_preference == "Men":
+        pref_genders = ["Male", "Nonbinary"]
+    elif gender_preference == "Women":
+        pref_genders = ["Female", "Nonbinary"]
+    else:
+        pref_genders = ["Male", "Female", "Nonbinary"]
+
+    return pref_genders
+
+
 def run_matching(scores: List[List], gender_id: List, gender_pref: List) -> List[Tuple]:
     """
     TODO: Implement Gale-Shapley stable matching!
@@ -21,7 +33,68 @@ def run_matching(scores: List[List], gender_id: List, gender_pref: List) -> List
             - What data structure can you use to take advantage of this fact when forming your matches?
         - This is by no means an exhaustive list, feel free to reach out to us for more help!
     """
-    matches = [()]
+
+    # pseudocode: https://drive.google.com/file/d/1ejGCXLGijnqLcUy2Cx8VuZWQmcaThisi/view
+
+    # initialize list of unmatched people
+    N = len(gender_id)
+    unmatched = list(range(N))
+
+    # matrix for whether proposer has considered match with acceptor
+    proposed = [([0] * N) for x in range(N)]
+
+    # initialize matches tuple list
+    matches = []
+
+    # while unmatched != []:
+    for person1 in range(N):
+        if unmatched == []:
+            break
+        for person in range(N):
+            person1 = unmatched[0]
+
+            # or person1 not in unmatched
+            if person1 == person or proposed[person1][person] == 1: 
+                continue               
+
+            # update scores in matrix to be 0 if genders incompatible
+            if gender_id[person1] not in pref_list(gender_pref[person]):
+                scores[person1][person] = 0
+                # has tried proposing
+                proposed[person1][person] = 1
+                continue
+
+            # if person is free
+            if scores[person1][person] != 0 and person in unmatched:
+                matches.append((person1, person))
+                unmatched.remove(person1)
+                unmatched.remove(person)
+                # has tried proposing
+                proposed[person1][person] = 1
+                break
+
+            # else if person prefers m to current match m2
+            if person not in unmatched:
+                item = [a for a in matches if a[0] == person]
+                if item != []:
+                    m2 = item[0][1]
+                    if scores[person][person1] > scores[person][m2]:
+                        matches.remove(item[0])
+                        matches.append((person1, person))
+                        unmatched.append(m2)
+                        unmatched.remove(person1)
+                        # has tried proposing
+                        proposed[person1][person] = 1
+                continue
+
+            else:
+                # has tried proposing
+                proposed[person1][person] = 1
+                break
+
+
+        # if successful, remove m from unmatched list, add match to matches
+    print(matches)
     return matches
 
 if __name__ == "__main__":
